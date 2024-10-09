ce3d81b58fe8c3ed658e4427a336397d649190de
Lukew292 <lukemw292@gmail.com>
2024-10-09 14:19:40 -0400

    Create ReadRate.m

---
+++ ReadRate.m
@@ -0,0 +1,35 @@
+clc
+clear all
+close all
+
+s = serialport("COM3","38400","DataBits",8,"StopBits",1);
+configureTerminator(s,"CR");
+
+% Read power
+%writeline(s,"N1,00");
+%power = readline(s);
+
+
+
+%%Read rate
+%start clock
+%start loop
+%send multi-tag read
+multiTagTable = zeros(1,2);
+for i = 1:20
+    multiTagRead = writeline(s,"U"); % Multi Tag EPC command writeline is used to send ASCII 
+    tagID = readline(s); % Read returned tag ID's
+    [row,col] = find(tagID)
+    %find tag id in second column of table
+    % if tag ID is in table
+        % first column in tag ID row incremented by 1
+    % if id is not in table, append new row to table, count = 1
+        newline = [1,tagID]
+        [multiTagTable]
+%create a table to store tag id's and read counts
+
+
+%create table: column 1 = numcounts, column = tag id
+%stop loop
+%stop clock
+%for specific tag, divide numcounts by time
