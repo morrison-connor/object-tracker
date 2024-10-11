ffdb32d1a615b4dd3f4c7c7e787db18513764548
Lukew292 <lukemw292@gmail.com>
2024-10-11 10:56:52 -0400

    Oct. 10 Updates

--- ReadRate.m
+++ ReadRate.m
@@ -16,10 +16,25 @@ configureTerminator(s,"CR");
 %start loop
 %send multi-tag read
 multiTagTable = zeros(1,2);
+
 for i = 1:20
     multiTagRead = writeline(s,"U"); % Multi Tag EPC command writeline is used to send ASCII 
-    tagID = readline(s); % Read returned tag ID's
-    [row,col] = find(tagID)
+    pause(0.1)  % delay to give time to serial command
+
+    % read serial response
+    readData = strings;
+    tagId = strings;
+    while s.NumBytesAvailable > 0
+        line = s.readline();
+        readData = [readData;line]; % Read returned tag ID's
+        tagId = readData(3:end);
+    end
+        tagIdIndex = find(multiTagTable == tagID);
+        if isempty(tagID_index)
+            table_entry = [1,tagID];
+            multiTagTable(end,1) = 1;
+            multiTagTable(end,2) = tagId;
+        end
     %find tag id in second column of table
     % if tag ID is in table
         % first column in tag ID row incremented by 1
