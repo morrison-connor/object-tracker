76cf417db3ab98ac254afb98c0dd82e581371557
Connor Morrison <connor.mrsn@gmail.com>
2024-10-30 11:42:27 -0400

    added print statement for debugging

--- demo_script.m
+++ demo_script.m
@@ -4,7 +4,7 @@ clear
 %% user inputs
 tagId = '3000E2806894000040319034C93BD2F2';
 
-nMeasurements = 200;
+nMeasurements = 100;
 antennaCycles = 10;
 windowSize = 6;  % how many data points to use in moving average
 antennaPorts = [1 4];  % input antenna ports used ie. [1 2 4]
@@ -20,13 +20,16 @@ s.configureTerminator("CR/LF");
 measurementNum = 0;
 totalRead = zeros(1, 2);
 while(true)
+    %input("Press any key to start")
     % 1. take a read ratio measurement
+    disp("1. take a read ratio measurement")
     measurementNum = measurementNum + 1;
-    for jj = 1:nMeasurements
-        for kk = 1:nAntennas
+    for ii = 1:floor(nMeasurements/(nAntennas*antennaCycles))
+        for jj = 1:nAntennas
 
             % a. switch antenna
-            antennaNum = antennaPorts(kk);
+            disp("a. switch antenna")
+            antennaNum = antennaPorts(jj);
             switch antennaNum
                 case 1
                     cmd = ["N9,N20", "N9,10"];
@@ -37,7 +40,7 @@ while(true)
                 case 4
                     cmd = ["N9,N22", "N9,10"];
             end
-            for ii = 1:length(cmd)
+            for kk = 1:length(cmd)
                s.writeline(cmd(1));  % Send 1st antenna switch command
                pause(0.1)
                s.readline();
@@ -50,32 +53,40 @@ while(true)
             end
 
             % b. take measurements
-            for ll = 1:antennaCycles
+            disp("b. take measurements")
+            for kk = 1:antennaCycles
                 % run RFID reader
                 readData = read_tags(s);
                 % check if target tag was detected
                 if any(contains(tagId, readData))  % looks for tagId in all returned data
-                    totalRead(kk) = totalRead(kk) + 1;
+                    totalRead(jj) = totalRead(jj) + 1;
                 end
             end
+            fprintf("Total measurements = %d\n", sum(totalRead))
         end
     end
 
     % 2. calculate the read ratio
-    for kk = 1:nAntennas
-        readRatio = totalRead(kk) / nMeasurements;
+    disp("2. calculate the read ratio")
+    for jj = 1:nAntennas
+        readRatio = totalRead(jj) / nMeasurements;
+        fprintf("Antenna %d readRatio = %0.2f\n", antennaPorts(jj), readRatio)
+
         % weightedAvg across windowSize
         if measurementNum < windowSize
-            measurements(kk, measurementNum) = readRatio;
-            weightedAvg(end) = mean(measurements(kk, 1:measurementNum));
+            measurements(jj, measurementNum) = readRatio;
+            weightedAvg(jj) = mean(measurements(jj, 1:measurementNum));
         else
             measurements(1:end-1) = measurements(2:end);
-            measurements(kk, end) = readRatio;
-            weightedAvg(end) = mean(measurements(kk));
+            measurements(jj, end) = readRatio;
+            weightedAvg(jj) = mean(measurements(jj));
         end
     end
 
-    % 3. use weightedAvg to determine position
+    % 3. use weightedAvg to determine position (for a 2 antenna system)
+    fprintf("Weighted Avg 1 = %0.2f\n", weightedAvg(1))
+    fprintf("Weighted Avg 2 = %0.2f\n", weightedAvg(2))
+    disp("3. use weightedAvg to determine position")
     delta = weightedAvg(1) - weightedAvg(2);
     if delta > 0
         disp("Turn Right")
@@ -84,4 +95,5 @@ while(true)
     else
         disp("Correct direction")
     end
+    pause(2);
 end
