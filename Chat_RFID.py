f68fc8b783dce217d9ba3e5391a0485817b12bf5
Connor Morrison <connor.mrsn@gmail.com>
2025-01-29 10:28:10 -0500

    ChatGPT code for RX processing
    Pasted a lot of ChatGPT for processing RX signals. Not tested and some sections verified to be incorrect. Also added a generic plotting function for debugging.

--- Chat_RFID.py
+++ Chat_RFID.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import adi
 import time
+from scipy.signal import butter, filtfilt
 
 def generate_waveform(data_bits, symbol_length, high_amp=2**14, low_amp=0):
     waveform = np.array([], dtype=np.float32)
@@ -56,11 +57,84 @@ def transmit_signal(sdr, signal, carrier_freq, fs):
     sdr.tx_hardwaregain_chan0 = -10
     sdr.tx([signal, signal])
 
-def receive_signal(sdr, carrier_freq, fs):
-    sdr.rx_enabled_channels = [0]  # TODO only using one channel for now
-    sdr.rx_lo = int(carrier_freq)
-    sdr.rx_rf_bandwidth = int(fs)
-    return sdr.rx()
+def butter_bandpass(lowcut, highcut, fs, order=4):
+    """Creates a Butterworth bandpass filter."""
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return b, a
+
+def apply_bandpass_filter(signal, lowcut, highcut, fs, order=5):
+    """Applies a bandpass filter to remove noise and unwanted frequencies."""
+    b, a = butter_bandpass(lowcut, highcut, fs, order)
+    return filtfilt(b, a, signal)
+
+def envelope_detection(signal):
+    """Extracts the envelope of the received signal using Hilbert transform."""
+    #generic_signal_plot("pre envelope", signal)
+    analytic_signal = np.abs(np.fft.ifft(np.fft.fft(signal)))
+    #generic_signal_plot("post envelope", signal)
+    return analytic_signal
+
+def fm0_decode(received_signal, fs, bit_rate):
+    """Decodes an FM0 encoded RFID response to recover binary data."""
+
+    # TODO entire function probably wrong
+
+    symbol_length = int(fs / bit_rate)
+    threshold = np.mean(received_signal)
+    bits = (received_signal > threshold).astype(int)
+    decoded_bits = []
+    for i in range(0, len(bits), symbol_length):
+        decoded_bits.append(bits[i])
+    return decoded_bits
+
+def extract_epc(decoded_bits):
+    """Extracts the EPC data from the decoded tag response."""
+    preamble = [1, 0, 1, 0]
+    index = -1
+    for i in range(len(decoded_bits) - len(preamble)):
+        if decoded_bits[i:i+len(preamble)] == preamble:
+            index = i + len(preamble)
+            break
+    if index == -1:
+        print("Preamble not found")
+        return None
+    return decoded_bits[index:index+16]  # RN16 (first response from the tag)
+
+def receive_and_process_signal(sdr, fs, bit_rate):
+    """Receives and processes RFID signals to identify EPC Gen2 tags."""
+    raw_signal = sdr.rx()
+    filtered_signal = apply_bandpass_filter(raw_signal, 100e3, 400e3, fs)
+    envelope = envelope_detection(filtered_signal)
+    decoded_bits = fm0_decode(envelope, fs, bit_rate)
+    epc_data = extract_epc(decoded_bits)
+    return epc_data
+
+def generic_signal_plot(title, signal):
+    t = np.linspace(0, (len(signal) - 1) / fs, len(signal))
+
+    plt.figure(figsize=(12, 8))
+    plt.subplot(2, 1, 1)
+    plt.plot(t, signal, color="purple")
+    plt.title(title + " - Time Domain")
+    plt.xlabel("Time (s)")
+    plt.ylabel("Amplitude")
+    plt.legend()
+    plt.grid()
+
+    freq = np.fft.fftfreq(len(signal), d=1/fs)
+    fft_signal = np.fft.fft(signal)
+    plt.subplot(2, 1, 2)
+    plt.plot(freq[:int(fs)//2], np.abs(fft_signal[:int(fs)//2]))
+    plt.title(title + " - Frequency Domain")
+    plt.xlabel("Frequency (Hz)")
+    plt.ylabel("Magnitude")
+    plt.grid()
+
+    plt.tight_layout()
+    plt.show()
 
 def plot_signals(tx_t, tx_signal, rx_t, rx_signal, fs):
     plt.figure(figsize=(12, 8))
@@ -128,15 +202,19 @@ sdr = adi.ad9361(uri='ip:192.168.2.1')
 sdr.rx_buffer_size = int(symbol_length * len(encoded_bits) * 2)  # adjust dynamically based on cmd length
 
 # Transmit signal
-for _ in range(10):  # send out 10 queries
-    transmit_signal(sdr, tx_signal, carrier_freq, fs)
-    time.sleep(0.01)  # allow time for tag to respond
+transmit_signal(sdr, tx_signal, carrier_freq, fs)
+time.sleep(0.01)  # allow time for tag to respond
 
 # Receive signals
-rx_signal = receive_signal(sdr, carrier_freq, fs)
+sdr.rx_enabled_channels = [0]  # TODO only using one channel for now
+sdr.rx_lo = int(carrier_freq)
+sdr.rx_rf_bandwidth = int(fs)
+epc_data = receive_and_process_signal(sdr, fs, bit_rate)
+if epc_data:
+    print("Detected EPC Data:", ''.join(map(str, epc_data)))
 
 # Time vectors for plotting
-tx_t = np.linspace(0, (len(tx_signal) - 1) / fs, len(tx_signal))
-rx_t = np.linspace(0, (len(rx_signal) - 1) / fs, len(rx_signal))
+#tx_t = np.linspace(0, (len(tx_signal) - 1) / fs, len(tx_signal))
+#rx_t = np.linspace(0, (len(rx_signal) - 1) / fs, len(rx_signal))
 
-plot_signals(tx_t, tx_signal, rx_t, rx_signal, fs)
+#plot_signals(tx_t, tx_signal, rx_t, rx_signal, fs)
